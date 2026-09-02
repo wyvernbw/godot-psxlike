6f9da9a1233bd929202ce8d342dd910fbffdb18a
wyvernbw <iamwyvern511@gmail.com>
2026-09-02 23:03:48 +0300

    fix plugin global shader init

--- addons/psxlike/psx_plugin.gd
+++ addons/psxlike/psx_plugin.gd
@@ -5,50 +5,64 @@ const PSX_LIGHT_SYSTEM := "PsxLightSystem"
 var shader_globals := [
 	{
 		"name": "ambient_light",
+		"type": "color",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_COLOR,
 		"default": Color.BLACK
 	},
 	{
 		"name": "fog_color",
+		"type": "color",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_COLOR,
 		"default": Color.LIGHT_GRAY
 	},
 	{
 		"name": "fog_enabled",
+		"type": "bool",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_BOOL,
 		"default": false
 	},
 	{
 		"name": "fog_far_plane",
+		"type": "float",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_FLOAT,
 		"default": 10.0
 	},
 	{
 		"name": "fog_near_plane",
+		"type": "float",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_FLOAT,
 		"default": 1.0
 	},
 	{
 		"name": "fog_kind",
+		"type": "int",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_INT,
 		"default": 1
 	},
 	{
 		"name": "light_count",
+		"type": "int",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_INT,
 		"default": 0
 	},
 	{
 		"name": "lights",
+		"type": "sampler2D",
 		"kind": RenderingServer.GLOBAL_VAR_TYPE_SAMPLER2D,
-		"default": Texture.new()
+		"default": ""
 	},
 ]
 
 func _enable_plugin() -> void:
 	add_autoload_singleton(PSX_LIGHT_SYSTEM, "./psx_light_system.gd")
 	for uniform in shader_globals:
-		RenderingServer.global_shader_parameter_add(uniform.name, uniform.kind, uniform.default)
+		var setting = "shader_globals/%s" % uniform.name
+		var data := {
+			"type": uniform.type,
+			"value": uniform.default,
+		}
+		ProjectSettings.set_setting(setting, data)
+	ProjectSettings.save()
 
 
 func _disable_plugin() -> void:
